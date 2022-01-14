de4e890eb46011d6f61c21bac9294fd69d713ec2
gitul0515 <gitul0515@gmail.com>
2022-01-14 16:49:58 +0900

    JS: navbar toggle btn & project UI freak

--- 자기소개_포트폴리오/main.js
+++ 자기소개_포트폴리오/main.js
@@ -1,10 +1,9 @@
 // navBar의 toggle-btn을 클릭하면 navbar__menu가 보여짐
-const navbarBtn = document.querySelector('.navbar__toggle-btn');
+const navbarToggle = document.querySelector('.navbar__toggle-btn');
 const navbarMenu = document.querySelector('.navbar__menu');
 
-navbarBtn.addEventListener('click', () => {
+navbarToggle.addEventListener('click', () => {
   navbarMenu.classList.toggle('show');
-  navBar.classList.add('opaque');
 })
 
 // 스크롤이 내려가면 navBar의 배경색을 green로 변경
@@ -34,9 +33,13 @@ navbarMenu.addEventListener('click', event => {
   
   scrollToElem(link);
 
-  curMenu.classList.remove('active');
-  curMenu = target;
-  curMenu.classList.add('active');
+  // 메뉴 버튼 스타일 처리
+  const prevMenu = document.querySelector('.navbar__menu__items.active');
+  prevMenu.classList.remove('active');
+  target.classList.add('active');
+
+  // 열려 있는 navbarMenu를 안 보이게 함
+  navbarMenu.classList.remove('show');
 })
 
 // home__contact을 클릭하면 contact 섹션으로 스크롤 이동
@@ -78,13 +81,17 @@ const projectBtns = document.querySelector('.projects__categories');
 const projectContainer = document.querySelector('.projects__work');
 const projects = [...document.querySelectorAll('.project')];
 
-// let current = categories.firstElementChild;
-
 projectBtns.addEventListener('click', e => {
   const target = e.target;
   const filter = target.dataset.filter;
   if (!filter) return;
+
+  // 버튼 처리
+  const prevButton = document.querySelector('.category__btn.active');
+  prevButton.classList.remove('active');
+  target.classList.add('active');
   
+  // 프로젝트 필터링
   projectContainer.classList.add('fade-out');
   setTimeout(() => {
     projects.forEach(project => {
