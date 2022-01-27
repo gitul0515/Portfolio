cf79480f403c9911c6b461ad06559a61de5fb039
gitul0515 <gitul0515@gmail.com>
2022-01-27 21:44:31 +0900

    bug&file click 오류 수정

--- 아이헤이트버그/main.js
+++ 아이헤이트버그/main.js
@@ -1,24 +1,21 @@
-const audioBgm = new Audio('./sound/bgm.mp3');
-const audioBug = new Audio('./sound/bug2.mp3');
-const audioFile = new Audio('./sound/file.mp3');
-audioSetting();
-
 const stageSetting = {
   1 : { time: 50, bugNum: 4 },
   2 : { time: 50, bugNum: 4 },
   3 : { time: 50, bugNum: 4 }
 }
-
 let stage = 1;
 let time = stageSetting[stage].time;
 let timeId = undefined;
 let bugNum = stageSetting[stage].bugNum;
-let heart = 3;
+let lifeNum = 3;
+const audioBgm = new Audio('./sound/bgm.mp3');
+const audioBug = new Audio('./sound/bug2.mp3');
+const audioFile = new Audio('./sound/file.mp3');
 
 const header = document.querySelector('.header');
 const stageDisplay = document.querySelector('.status__stage');
 const timeDisplay = document.querySelector('.status__time');
-const hearts = document.querySelectorAll('.status__heart');
+const lifePoints = document.querySelectorAll('.status__life-point');
 const content = document.querySelector('.content');
 const bugImgs = document.querySelectorAll('.bug__img');
 const fileImgs = document.querySelectorAll('.file__img');
@@ -29,51 +26,33 @@ const overlayBtn = document.querySelector('.overlay__btn');
 const overlayMain = document.querySelector('.overlay__main-title');
 const overlaySub = document.querySelector('.overlay__sub-title');
 
-// main
-overlayBtn.addEventListener('click', () => {
-  gameStart();
-});
-
-bugImgAddEvent();
-fileImgAddEvent();
-
-
-
-btnPause.addEventListener('click', () => {
-  if (btnPauseIcon.className === 'fas fa-pause') {
-    clearInterval(timeId);
-    btnPauseIcon.classList.remove('fa-pause');
-    btnPauseIcon.classList.add('fa-play');
-  } else {
-    timeId = reduceTime();
-    btnPauseIcon.classList.remove('fa-play');
-    btnPauseIcon.classList.add('fa-pause');
-  }
-});
-/*
-  btnPauseIcon이 'fas fa-pause'인 경우, 
-  시간을 중지하고 icon을 'fas fa-play'로 변경한다.
-  btnPauseIcon이 'fas fa-play'인 경우,
-  시간을 흐르게하고 icon을 'fas fa-pause'로 변경한다.
-*/
+audioSetting();
+gameStart();
+addEvent();
 
 function gameStart() {
+  overlayBtn.addEventListener('click', () => {
+    stageStart();
+  });
+}
+
+function stageStart() {
   stageDisplay.textContent = `Stage ${stage}`;
   time = stageSetting[stage].time;
   updateTimeText();
   timeId = reduceTime(); // reduce every 1 second
   bugNum = stageSetting[stage].bugNum;
 
-  showBugs(); showFiles();
+  showBugImgs(); showFileImgs();
   overlayHide();
   audioBgm.play();
 }
 
-function showBugs() {
+function showBugImgs() {
   bugImgs.forEach(bugImg => {
     bugImg.classList.remove('hidden');
 
-    // 부모 영역(.bugs)을 기준으로 0% ~ 85% 위치에 표시
+    // 부모 영역(.bugs)을 기준으로 0 ~ 85% 위치에 표시
     const randomXPos = Math.floor(Math.random() * 86);
     const randomYPos = Math.floor(Math.random() * 86);
     bugImg.style.left = `${randomXPos}%`;
@@ -81,11 +60,11 @@ function showBugs() {
   });
 }
 
-function showFiles() {
+function showFileImgs() {
   fileImgs.forEach(fileImg => {
     fileImg.classList.remove('hidden');
 
-    // 부모 영역(.files)을 기준으로 0% ~ 85% 위치에 표시
+    // 부모 영역(.files)을 기준으로 0 ~ 85% 위치에 표시
     const randomXPos = Math.floor(Math.random() * 86);
     const randomYPos = Math.floor(Math.random() * 86);
     fileImg.style.left = `${randomXPos}%`;
@@ -100,14 +79,14 @@ function gameOver() {
   overlaySub.setAttribute('src', './img/cry.png');
   overlayBtn.textContent = 'Replay?';
 
-  hearts.forEach(heart => {
-    heart.classList.remove('hidden');
+  lifePoints.forEach(lifePoint => {
+    lifePoint.classList.remove('hidden');
   });
 
   stage = 1;
   time = stageSetting[stage].time;
   bugNum = stageSetting[stage].bugNum;
-  heart = 3;
+  lifeNum = 3;
   clearInterval(timeId);
 }
 
@@ -141,7 +120,6 @@ function reduceTime() {
 function updateTimeText() {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
-
   timeDisplay.textContent = `${minutes} : ${seconds}`;
 }
 
@@ -164,6 +142,12 @@ function audioFilePlay() {
   audioFile.play();
 }
 
+function addEvent() {
+  bugImgAddEvent();
+  fileImgAddEvent();
+  btnPauseAddEvent();
+}
+
 function bugImgAddEvent() {
   bugImgs.forEach(bugImg => {
     bugImg.addEventListener('click', () => {
@@ -171,8 +155,7 @@ function bugImgAddEvent() {
       bugImg.classList.add('hidden');
       bugNum--;
       if (!bugNum) {
-        clearInterval(timeId);
-  
+        clearInterval(timeId); 
         overlayShow();
   
         stage++;
@@ -195,12 +178,28 @@ function fileImgAddEvent() {
   fileImgs.forEach(fileImg => {
     fileImg.addEventListener('click', () => {
       fileImg.classList.add('hidden');
-      heart--;
-      hearts[heart].classList.add('hidden');
-      if (heart === 0) {
+      lifeNum--;
+      lifePoints[lifeNum].classList.add('hidden');
+      if (lifeNum === 0) {
         gameOver();
       }
       audioFilePlay();
     });
+  });
+}
+
+function btnPauseAddEvent() {
+  btnPause.addEventListener('click', () => {
+    if (btnPauseIcon.className === 'fas fa-pause') {
+      // Time pause
+      clearInterval(timeId);
+      btnPauseIcon.classList.remove('fa-pause');
+      btnPauseIcon.classList.add('fa-play');
+    } else {
+      // Time play (reduce)
+      timeId = reduceTime();
+      btnPauseIcon.classList.remove('fa-play');
+      btnPauseIcon.classList.add('fa-pause');
+    }
   });
 }
